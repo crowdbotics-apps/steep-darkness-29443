db7e0f27b5c4a0befff423463cc80a1e28e175e7
Crowdbotics <team@crowdbotics.com>
2021-08-04 07:21:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "steep_darkness_29443Identifier",
+  url: "https://steep_darkness_29443Identifier.botics.co",
+  api: "https://steep_darkness_29443Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
